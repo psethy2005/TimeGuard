a364a1586574b97ef6f599ded5d176f2292d8fd4
jsuhe <jsuhe@terpmail.umd.edu>
2025-03-17 18:01:19 -0400

    Test push

--- lib/TimeControl.dart
+++ lib/TimeControl.dart
@@ -1,1 +1,24 @@
 // This will be the time settings
+
+import 'package:flutter/material.dart';
+
+void main {
+    runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Container(
+                child: const Text('Time'),
+                margin: const EdgeInsets.all(100)
+                
+            )
+        )
+    );
+  }
+}
